bd4430f580e02882f9a800cc9ada7fa6a040d156
sofiahernandezas <sofiahernandezastig@gmail.com>
2024-08-17 21:02:11 -0300

    Evitar burbujeo
    Se quita el atributo onclick en el HTML y se modifiva el JS para evitar el burbujeo

--- eventos.js
+++ eventos.js
@@ -1,12 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", function() { //DOMContentLoaded permite que el script no se cargue antes que el DOM
     var element = document.getElementById("div");
-    
+    var button = element.querySelector("button"); // Selecciona el botón dentro del div
 
-    function myFunction() {
+
+    function myFunction(event) {
         alert("Hola! Soy el div");
+    
+    }
+
+    function buttonClick(event) {
+        event.stopPropagation(); // Evita que el clic en el botón se propague al div
+        alert('Hola!');
+        
     }
 
     element.addEventListener("click", myFunction);
- 
+    button.addEventListener("click", buttonClick);
 });
